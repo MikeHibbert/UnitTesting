5df97a629be6dc06f1c3fc303ea78a45dd4caba0
Mike Hibbert <hibbert.michael@gmail.com>
2015-10-29 09:56:27 +0000

    added more unit tests

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -11,7 +11,7 @@ class TestVendingMachine(unittest.TestCase):
         self.assertEqual(give_change(.04), [.02, .02])
 
     def test_unavailable_item(self):
-        """if user asks for an item that's unavailable, they should not be given the       item,  and their money should be returned"""
+        """if user asks for an item that's unavailable, they should not be given the item, and their money should be returned"""
         item, change, _ = give_item_and_change('crisps', .50)
         self.assertIsNone(item)
         self.assertEqual(change, 0.5)
@@ -20,6 +20,23 @@ class TestVendingMachine(unittest.TestCase):
         """tests is amount is less than the cost"""
         item, change, _ = give_item_and_change('coke', .50)
         self.assertIsNone(item)
+        self.assertEqual(change, 0.5)
+
+    def test_give_item_and_change_amount_works_with_correct_money(self):
+        """tests is amount is less than the cost"""
+        item, change, _ = give_item_and_change('coke', .73)
+        self.assertIsNotNone(item)
+        self.assertEqual(item, 'coke')
+        self.assertEqual(change, [])
+
+    def test_give_item_and_change_amount_works_with_correct_money(self):
+        """tests is amount is less than the cost"""
+        item, change, _ = give_item_and_change('coke', 2.0)
+        self.assertIsNotNone(item)
+        self.assertEqual(item, 'coke')
+        self.assertEqual(change, [1,.20, .05, .02])
+
+#   AssertionError: [] != 0.73
 
 
 
